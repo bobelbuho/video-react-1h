894ef7c273a60b6e359641e3d6a161b3860c4498
bobelbuho <bob.vaz@hotmail.fr>
2023-06-01 22:52:39 +0200

    30.55

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import { SayHello } from "./SayHello.";
+
 import { Tweet } from "./Tweet";
 
 function App(){
   return (
     <div>
-   <Tweet />
+      <p>I'm App component</p>
+    <div className="tweet-container">
+   <Tweet name="Melvyn" content="je vais bien" like={1000}/>
+   <Tweet name="Didier" content="Cool" like={1000}/>
+   <Tweet name="Luca" content="Super" like={1000}/>
+   <Tweet name="Jean" content="Mood swing" like={1000}/>
+   </div>
    </div>
   );
 }
--- src/Tweet.jsx
+++ src/Tweet.jsx
@@ -1,7 +1,12 @@
-export function Tweet(){
-    return (<div className="tweet">
-        <h3>name</h3>
-        <p>content</p>
-        <p>12 v</p>
+export function Tweet(name, content, like){
+
+    const onLike = () =>{
+        console.log("LIKE", props.name);
+    }
+    return (
+        <div className="tweet">
+        <h3>{name}</h3>
+        <p>{content}</p>
+        <button onClick={() => onLike()}>{like} v</button>
     </div>)
 }
