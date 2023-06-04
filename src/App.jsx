9b8ca99ab5220f5ccc0dfe3120a51f65311faabb
bobelbuho <bob.vaz@hotmail.fr>
2023-06-04 21:11:55 +0200

    user

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
-
+import { userState } from "react";
 import { Tweet } from "./Tweet";
 
-function App(){
+function App() {
+  let [tweets, setTweets] = userState();
+  let [username, setUsername] = userState("Melvin");
+  const addLetter = () => {
+    username += "a"
+    console.log(username);
+  }
   return (
     <div>
-      <p>I'm App component</p>
-    <div className="tweet-container">
-   <Tweet name="Melvyn" content="je vais bien" like={1000}/>
-   <Tweet name="Didier" content="Cool" like={1000}/>
-   <Tweet name="Luca" content="Super" like={1000}/>
-   <Tweet name="Jean" content="Mood swing" like={1000}/>
-   </div>
-   </div>
+      <p>{username}</p>
+      <button onClick={addLetter}>Ajoute une lettre</button>
+      <div className="tweet-container">
+        <Tweet name="Melvyn" content="je vais bien" like={1000} />
+        <Tweet name="Didier" content="Cool" like={1000} />
+        <Tweet name="Luca" content="Super" like={1000} />
+        <Tweet name="Jean" content="Mood swing" like={1000} />
+      </div>
+    </div>
   );
 }
 export default App;
